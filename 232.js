9ff26d1518887ba50bb495a696564ee2fb3324c2
Arslan <mengzhihunqu@163.com>
2021-05-26 13:41:59 +0800

    232

---
+++ 232.js
@@ -0,0 +1,88 @@
+/**
+ * 232. Implement Queue using Stacks
+ * Implement a first in first out (FIFO) queue using only two stacks. The implemented queue should support all the functions of a normal queue (push, peek, pop, and empty).
+
+Implement the MyQueue class:
+
+void push(int x) Pushes element x to the back of the queue.
+int pop() Removes the element from the front of the queue and returns it.
+int peek() Returns the element at the front of the queue.
+boolean empty() Returns true if the queue is empty, false otherwise.
+Notes:
+
+You must use only standard operations of a stack, which means only push to top, peek/pop from top, size, and is empty operations are valid.
+Depending on your language, the stack may not be supported natively. You may simulate a stack using a list or deque (double-ended queue) as long as you use only a stack's standard operations.
+Follow-up: Can you implement the queue such that each operation is amortized O(1) time complexity? In other words, performing n operations will take overall O(n) time even if one of those operations may take longer.
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/implement-queue-using-stacks
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+/**
+ * Initialize your data structure here.
+ */
+var MyQueue = function () {
+  //in
+  this.stackA = [];
+  //out
+  this.stackB = [];
+};
+
+/**
+ * Push element x to the back of queue.
+ * @param {number} x
+ * @return {void}
+ */
+MyQueue.prototype.push = function (x) {
+  //直接压入A栈中
+  this.stackA.push(x);
+};
+
+/**
+ * Removes the element from in front of queue and returns that element.
+ * @return {number}
+ */
+MyQueue.prototype.pop = function () {
+  //如果B栈为空，则从A栈逐步压入元素进入B栈
+  if (!this.stackB.length) {
+    while (this.stackA.length) {
+      this.stackB.push(this.stackA.pop());
+    }
+  }
+  return this.stackB.pop();
+};
+
+/**
+ * Get the front element.
+ * @return {number}
+ */
+MyQueue.prototype.peek = function () {
+  if (!this.stackB.length) {
+    while (this.stackA.length) {
+      this.stackB.push(this.stackA.pop());
+    }
+  }
+  return this.stackB[this.stackB.length - 1];
+};
+
+/**
+ * Returns whether the queue is empty.
+ * @return {boolean}
+ */
+MyQueue.prototype.empty = function () {
+  //如果两个栈都为空则说明队列为空返回true,否则返回false
+  if (!this.stackA.length && !this.stackB.length) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * var obj = new MyQueue()
+ * obj.push(x)
+ * var param_2 = obj.pop()
+ * var param_3 = obj.peek()
+ * var param_4 = obj.empty()
+ */
